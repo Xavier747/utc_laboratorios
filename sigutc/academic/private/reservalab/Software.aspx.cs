9efa6a82cc668b6c6a5d59e9ed8216e9744dc544
henrychol <henry.choloquinga3190@utc.edu.ec>
2025-05-09 00:58:53 -0500

    lib

--- sigutc/academic/private/reservalab/Software.aspx.cs
+++ sigutc/academic/private/reservalab/Software.aspx.cs
@@ -10,7 +10,6 @@ using System.Data.SqlClient;
 using System.Configuration;
 using System.Data;
 using ClassLibraryLaboratorios;
-using ClassLibraryTesis;
 using System.Web.Services;
 
 public partial class academic_private_reservaLab_Software : System.Web.UI.Page
@@ -18,6 +17,7 @@ public partial class academic_private_reservaLab_Software : System.Web.UI.Page
     LAB_SOFTWARE software1 = new LAB_SOFTWARE();
     UB_SEDES sede = new UB_SEDES();
     UB_FACULTADES facultad = new UB_FACULTADES();
+
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -29,6 +29,7 @@ public partial class academic_private_reservaLab_Software : System.Web.UI.Page
             cargarSede();
             cargarFacultad();
             cargarSoftware();
+            cargarTipoLicencia();
         }
     }
 
@@ -108,6 +109,158 @@ public partial class academic_private_reservaLab_Software : System.Web.UI.Page
             lblMsg.Text = software1.msg;
         }
     }
+
+    public void cargarTipoLicencia()
+    {
+        ddlTipo.Items.Add(new ListItem("Propietario", "Propietario"));
+        ddlTipo.Items.Add(new ListItem("Libre", "Libre"));
+
+        ddlTipo.Visible = ddlTipo.SelectedValue == "Propietario" ? true : false;
+    }
+
+    //guardar nuevo software
+
+    protected void btnGuardar_Click(object sender, EventArgs e)
+    {
+        decimal precioUnitario = txtCosto.Text != "" ? decimal.Parse(txtCosto.Text) : 0;
+        string rutaCarpeta = crearDirectorio();
+
+        software1.strNombre_sof = txtNombre.Text;
+        software1.strTipoLicencia_sof = ddlTipo.SelectedValue;
+        software1.strNombreLicencia_sof = txtNombreLicencia.Text;
+        software1.intCantidad_sof = int.Parse(txtCantidad.Text);
+        software1.decCostoUnitario_sof = precioUnitario;
+        software1.decCostoTotal_sof = decimal.Parse(txtCantidad.Text) * precioUnitario;
+        software1.strDescripcion_sof = txtDescripcion.Text;
+        software1.strUrl_sof = txtLink.Text;
+        software1.dtFechaRegistro_sof = DateTime.Now;
+        software1.dtFecha_log = DateTime.Now;
+        software1.strUser_log = Context.User.Identity.Name;
+        software1.bitEstado_sof = true;
+
+
+        software1.strObs1_sof = string.Empty;
+        software1.strObs2_sof = string.Empty;
+        software1.bitObs1_sof = false;
+        software1.bitObs2_sof = false;
+        software1.decObs1_sof = -1;
+        software1.decObs2_sof = -1;
+        software1.dtObs1_sof = DateTime.Parse("1900-01-01");
+        software1.dtObs2_sof = DateTime.Parse("1900-01-01");
+
+        software1.strCod_Fac = ddlFacultad.SelectedValue;
+        software1.strCod_Sede = ddlSede.SelectedValue;
+        software1.strCod_sof = generarIdSoft();
+
+        
+
+        if (fulImg1.HasFile)
+        {
+            try
+            {
+                string filename = Path.GetFileNameWithoutExtension(fulImg1.FileName);
+                string extension = Path.GetExtension(fulImg1.FileName);
+                string newFilename = filename + extension;
+                string path = Path.Combine(rutaCarpeta, newFilename);
+
+                int counter = 1;
+                while (File.Exists(path))
+                {
+                    newFilename = $"{filename}_{counter}{extension}";
+                    path = Path.Combine(rutaCarpeta, newFilename);
+                    counter++;
+                }
+
+                fulImg1.SaveAs(path);
+                software1.strImagen_sof = path;
+            }
+            catch (Exception ex)
+            {
+                Response.Write("La carga falló: " + ex.Message);
+            }
+        }
+
+        int registro = software1.AddLAB_SOFTWARE(software1);
+
+        string title = registro != -1 ? software1.msg : software1.msg;
+        string icon = registro != -1 ? "success" : "error";
+        string ruta = "Software.aspx";
+
+        string script = $"showAlertAndReload('{title}', '{icon}', '{ruta}');";
+        ClientScript.RegisterStartupScript(this.GetType(), "ShowAlert", script, true);
+    }
+
+    private string crearDirectorio()
+    {
+        string rutaCarpeta = "";
+        try
+        {
+            // Ruta que deseas crear
+            rutaCarpeta = @"C:\images\Software";
+
+            // Validar si la carpeta ya existe
+            if (!Directory.Exists(rutaCarpeta))
+            {
+                // Crear la carpeta
+                Directory.CreateDirectory(rutaCarpeta);
+            }
+
+            string rutaParaBD = rutaCarpeta;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ocurrió un error al crear la carpeta: " + ex.Message);
+        }
+
+        return rutaCarpeta;
+    }
+
+    private string generarIdSoft()
+    {
+        string facultadId = ddlFacultad.SelectedValue;
+        string sedeId = ddlSede.SelectedValue;
+        string frase = txtNombre.Text.ToUpper();
+        string[] palabras = frase.Split(' ');
+
+        List<string> partes = new List<string>();
+
+        foreach (string palabra in palabras)
+        {
+            // Si tiene más de 5 caracteres, tomamos solo los primeros 5
+            string parte = palabra.Length > 3 ? palabra.Substring(0, 3) : palabra;
+            partes.Add(parte);
+        }
+
+        string resultado = string.Join("", partes);
+        resultado = sedeId + "_" + facultadId + "_" + resultado;
+
+        string codSoft = comprobarRegistro(resultado);
+
+        return codSoft;
+    }
+
+    public string comprobarRegistro(string resultado)
+    {
+        int acum = 0;
+
+        while (true)
+        {
+            var tablaDatos = software1.LoadLAB_SOFTWARE("xEstado", resultado, "", "", "");
+
+            if (tablaDatos.Count > 0)
+            {
+                ++acum;
+                resultado = resultado + "_" + acum;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return resultado;
+    }
+
     protected void btnNuevo_Click(object sender, EventArgs e)
     {
         Response.Redirect("~/academic/private/reservalab/NuevoSoftware.aspx");
@@ -143,5 +296,25 @@ public partial class academic_private_reservaLab_Software : System.Web.UI.Page
         string rutaImagen = btn.CommandArgument;
         vistaCompletaImagen.ImageUrl = rutaImagen;
         ScriptManager.RegisterStartupScript(this, GetType(), "OpenModal", "$('#view-image').modal('show');", true);
+    }
+
+    protected void ddlSede_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ddlTipo_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ddlSedeAct_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnActualizar_Click(object sender, EventArgs e)
+    {
+
     }
 }
