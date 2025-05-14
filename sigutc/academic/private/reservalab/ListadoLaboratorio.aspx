9ff0a718d89936826bd207e170cb5448ac4577a6
henrychol <henry.choloquinga3190@utc.edu.ec>
2025-05-13 21:44:52 -0500

    Alucardd

---
+++ sigutc/academic/private/reservalab/ListadoLaboratorio.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPageNuevo.master" AutoEventWireup="true" CodeFile="ListadoLaboratorio.aspx.cs" Inherits="academic_private_reservalab_ListadoLaboratorio" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+    <link href="../../../Styles/Nuevo/assets/css/laboratorio-style.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="TitleContent" Runat="Server">
+    Listado de laboratorios
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" Runat="Server">     
+    <!-- GridView para listar laboratorios -->
+    <div class="table-responsive">
+        <asp:GridView ID="gvLaboratorios" runat="server" AutoGenerateColumns="false" AllowPaging="True" PageSize="10" OnPageIndexChanging="gvLaboratorios_PageIndexChanging" CssClass="table table-striped table-bordered" OnRowCommand="gvLaboratorios_RowCommand">
+            <Columns>
+                
+                 <asp:BoundField DataField="strNombre_lab" HeaderText="Nombre" />
+                <asp:BoundField DataField="strUbicacion_lab" HeaderText="Ubicación" />
+                <asp:TemplateField HeaderText="Imagen 1">
+                    <ItemTemplate>
+                        <div class="content">
+                            <div class="content-img">
+                                <!--Ruta de la imagen envia desde el codigo para mostrar la imagen -->
+                                <asp:Image ID="imgLaboratorio1" runat="server" ImageUrl='<%# "ImageHandlerLaboratorio.ashx?image=" + System.IO.Path.GetFileName(Eval("strFotografia1_lab").ToString()) %>' />
+                            </div>
+                            <br />
+                            <!--Boton para mostrar la imagen en grande atraves de una ventana modal -->
+                            <asp:Button ID="btnViewImage1" CssClass="btn btn-info" OnClick="btnViewImage1_Click" CommandArgument='<%# "ImageHandlerLaboratorio.ashx?image=" + System.IO.Path.GetFileName(Eval("strFotografia1_lab").ToString()) %>' runat="server" Text="Ver Imagen" />
+                        </div>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                
+                
+                <asp:TemplateField ShowHeader="False" HeaderText="Accion">
+                    <ItemTemplate>
+                        <div style="display:flex;">
+                            
+                            
+                            <!--Boton para eliminar el laboratorio-->
+                            <asp:Button ID="btnVerReservaciones" runat="server" CommandName="VerReservaciones" CssClass="btn btn-danger" CommandArgument ='<%# Eval("strCod_lab") %>' OnClientClick="return confirm('¿Seguro que desea eliminar?');" Text="VerReservaciones" />&nbsp;&nbsp;
+                           
+                          
+                        </div>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView> 
+    </div>   
+    <asp:Label ID="lblMsg" runat="server" Text=""></asp:Label>       
+     <!-- Ventana Modal Imagen-->
+    <div class="modal fade" id="view-image" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog modal-lg" role="document" style="margin: 30px auto !important; left: 0% !important;">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                    <h4 class="modal-title" id="modalVisualizarLab">Visualizar laboratorio</h4>
+                </div>
+                <div class="modal-body">
+                    <asp:Image ID="vistaCompletaImagen" runat="server" style="width:100%;" AlternateText="hola"/>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="FooterContent" Runat="Server">
+</asp:Content>
+
---
+++ sigutc/academic/private/reservalab/ListadoLaboratorio.aspx.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using ClassLibraryLaboratorios;
+using ClassLibraryTesis;
+using System.Web.Configuration;
+
+public partial class academic_private_reservalab_ListadoLaboratorio : System.Web.UI.Page
+{
+    //Definicion de las variable de coneccion con la base de datos
+    SqlConnection conexion = new SqlConnection(WebConfigurationManager.AppSettings["conexionBddProductos"]);
+
+    LAB_LABORATORIOS laboratorio2 = new LAB_LABORATORIOS();
+    LAB_RESPONSABLE responsable1 = new LAB_RESPONSABLE();
+    LAB_SOFTWARE software1 = new LAB_SOFTWARE();
+    LAB_LABSOFTWARE softLab = new LAB_LABSOFTWARE();
+    LAB_EXCLUSIVO labExc = new LAB_EXCLUSIVO();
+    LAB_TIPO tipoLaboratorio1 = new LAB_TIPO();
+    UB_FACULTADES facultad = new UB_FACULTADES();
+    UB_SEDES sede = new UB_SEDES();
+
+    private Random rand = new Random();
+    public static List<string> softwareSeleccionado;
+    public static List<string> nuevosSoftwares;
+    public static List<string> softwaresActuales;
+    private string filtro;
+    private string title;
+    private string icon;
+
+    //Metodo principal de la pagina
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Context.User.Identity.Name == "") Response.Redirect("~/academic/private/Login.aspx");
+
+        if (!IsPostBack)
+        {
+            //llamado a los metodos que se ejecuta al iniciar la pagina     
+            cargarTabla();
+
+           
+           
+            cargarCampoAmplio();
+        }
+    }
+    protected void gvLaboratorios_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvLaboratorios.PageIndex = e.NewPageIndex;
+        cargarTabla();
+    }
+
+    protected void gvLaboratorios_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        if (e.CommandName == "Select")
+        {
+            string codLab = e.CommandArgument.ToString();
+
+
+            // Muestra el modal para actualizar los datos
+            ScriptManager.RegisterStartupScript(this, GetType(), "OpenModal", "$('#form_actualizar').modal('show');", true);
+        }
+        if (e.CommandName == "Eliminar")
+        {
+            string codLab = e.CommandArgument.ToString();
+            string dtFecha_log = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff");
+            string strUser_log = Context.User.Identity.Name;
+
+            //Carga los detalles del laboratorio según el ID seleccionado
+            laboratorio2.DelLAB_LABORATORIOS("xPkLab", codLab, dtFecha_log, strUser_log, "");
+
+            string title = laboratorio2.resultado ? laboratorio2.msg : laboratorio2.msg;
+            string icon = laboratorio2.resultado ? "success" : "error";
+
+            string script = $"showAlertAndReload('{title}', '{icon}');";
+            ClientScript.RegisterStartupScript(this.GetType(), "ShowAlert", script, true);
+        }
+        if (e.CommandName == "Laboratoristas")
+        {
+            string codLab = e.CommandArgument.ToString();
+      
+        }
+        if (e.CommandName == "Carrera")
+        {
+            Session["laboratorioId"] = e.CommandArgument.ToString();
+            Response.Redirect("LaboratorioCarrera.aspx");
+        }
+    }
+    public void cargarTabla()
+    {
+        laboratorio2.strCod_Fac = Session["ROL"].ToString() == "ADMINISTRADOR" ? "" : Context.User.Identity.Name;
+        string tipoConsulta = Session["ROL"] != null && Convert.ToString(Session["ROL"]) == "ADMINISTRADOR" ? "ALL" : "xIdPersonal";
+        string cedula = Context.User.Identity.Name;
+
+        var tablaDatos = laboratorio2.LoadLAB_LABORATORIOS(tipoConsulta, cedula, "", "", "");
+        if (tablaDatos != null && tablaDatos.Count > 0)
+        {
+            gvLaboratorios.DataSource = tablaDatos;
+            gvLaboratorios.DataBind();
+        }
+    }
+    public void cargarCampoAmplio()
+    {
+        string tipoConsulta = "ALL";
+
+        SqlCommand comandoConsulta = new SqlCommand("SIGUTC_GetAREAC", conexion);
+        comandoConsulta.Parameters.AddWithValue("@Comodin", tipoConsulta);
+        comandoConsulta.Parameters.AddWithValue("@FILTRO1", "");
+        comandoConsulta.Parameters.AddWithValue("@FILTRO2", "");
+        comandoConsulta.Parameters.AddWithValue("@FILTRO3", "");
+        comandoConsulta.Parameters.AddWithValue("@FILTRO4", "");
+        comandoConsulta.CommandType = CommandType.StoredProcedure;
+        try
+        {
+            this.conexion.Open();
+            SqlDataAdapter adaptadorAlbum = new SqlDataAdapter(comandoConsulta);
+            DataTable dt = new DataTable();
+            adaptadorAlbum.Fill(dt);
+
+            foreach (DataRow row in dt.Rows)
+            {
+               
+              
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write("TIENES UN ERROR: " + ex.Message);
+        }
+        conexion.Close();
+    }
+    protected void btnViewImage1_Click(object sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+        vistaCompletaImagen.ImageUrl = btn.CommandArgument;
+        ScriptManager.RegisterStartupScript(this, GetType(), "OpenModal", "$('#view-image').modal('show');", true);
+    }
+
+    protected void btnViewImage2_Click(object sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+        vistaCompletaImagen.ImageUrl = btn.CommandArgument;
+        ScriptManager.RegisterStartupScript(this, GetType(), "OpenModal", "$('#view-image').modal('show');", true);
+    }
+
+}
+
