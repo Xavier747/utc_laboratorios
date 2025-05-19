c72686c1e14ab926916f4eb2bd32c75171c56a99
henrychol <henry.choloquinga3190@utc.edu.ec>
2025-05-19 14:52:11 -0500

    Alucard

--- sigutc/academic/private/reservalab/InformacionLaboratorios.aspx.cs
+++ sigutc/academic/private/reservalab/InformacionLaboratorios.aspx.cs
@@ -51,52 +51,52 @@ public partial class academic_private_reservalab_InformacionLaboratorios : Syste
     }
 }
 
-class Personal
-{
-    public string CEDULA_ALU { get; set; }
-    public string NOMBRE_ALU { get; set; }
-    public string CORREO_ALU { get; set; }
-    public string IMAGEN_ALU { get; set; }
-
-    public Personal() { }
-
-    public List<Personal> LoadPersonal(string comodin, string filtro1, string filtro2, string filtro3, string filtro4)
+    class Personal
     {
-        var listPersonal = new List<Personal>();
+        public string CEDULA_ALU { get; set; }
+        public string NOMBRE_ALU { get; set; }
+        public string CORREO_ALU { get; set; }
+        public string IMAGEN_ALU { get; set; }
 
-        SqlConnection conexion = new SqlConnection(WebConfigurationManager.AppSettings["conexionBddProductos"]);
-        SqlCommand comandoConsulta = new SqlCommand("SIGUTC_GetPERSONAL", conexion);
-        comandoConsulta.Parameters.AddWithValue("@Comodin", comodin);
-        comandoConsulta.Parameters.AddWithValue("@FILTRO1", filtro1);
-        comandoConsulta.Parameters.AddWithValue("@FILTRO2", filtro2);
-        comandoConsulta.Parameters.AddWithValue("@FILTRO3", filtro3);
-        comandoConsulta.Parameters.AddWithValue("@FILTRO4", filtro4);
-        comandoConsulta.CommandType = CommandType.StoredProcedure;
-        try
+        public Personal() { }
+
+        public List<Personal> LoadPersonal(string comodin, string filtro1, string filtro2, string filtro3, string filtro4)
         {
-            conexion.Open();
-            SqlDataAdapter adaptadorAlbum = new SqlDataAdapter(comandoConsulta);
-            DataTable dt = new DataTable();
-            adaptadorAlbum.Fill(dt);
+            var listPersonal = new List<Personal>();
 
-            foreach (DataRow row in dt.Rows)
+            SqlConnection conexion = new SqlConnection(WebConfigurationManager.AppSettings["conexionBddProductos"]);
+            SqlCommand comandoConsulta = new SqlCommand("SIGUTC_GetPERSONAL", conexion);
+            comandoConsulta.Parameters.AddWithValue("@Comodin", comodin);
+            comandoConsulta.Parameters.AddWithValue("@FILTRO1", filtro1);
+            comandoConsulta.Parameters.AddWithValue("@FILTRO2", filtro2);
+            comandoConsulta.Parameters.AddWithValue("@FILTRO3", filtro3);
+            comandoConsulta.Parameters.AddWithValue("@FILTRO4", filtro4);
+            comandoConsulta.CommandType = CommandType.StoredProcedure;
+            try
             {
-                listPersonal.Add(
-                    new Personal
-                    {
-                        CEDULA_ALU = row["CEDULA_ALU"].ToString(),
-                        NOMBRE_ALU = row["APELLIDO_ALU"].ToString() + " " + row["APELLIDOM_ALU"].ToString() + " " + row["NOMBRE_ALU"].ToString(),
-                        IMAGEN_ALU = row["IMAGEN_ALU"].ToString(),
-                        CORREO_ALU = row["CORREO_ALU"].ToString()
-                    }
-                );
+                conexion.Open();
+                SqlDataAdapter adaptadorAlbum = new SqlDataAdapter(comandoConsulta);
+                DataTable dt = new DataTable();
+                adaptadorAlbum.Fill(dt);
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    listPersonal.Add(
+                        new Personal
+                        {
+                            CEDULA_ALU = row["CEDULA_ALU"].ToString(),
+                            NOMBRE_ALU = row["APELLIDO_ALU"].ToString() + " " + row["APELLIDOM_ALU"].ToString() + " " + row["NOMBRE_ALU"].ToString(),
+                            IMAGEN_ALU = row["IMAGEN_ALU"].ToString(),
+                            CORREO_ALU = row["CORREO_ALU"].ToString()
+                        }
+                    );
+                }
             }
+            catch (Exception ex)
+            {
+                Console.Write("TIENES UN ERROR: " + ex.Message);
+            }
+            conexion.Close();
+            return listPersonal;
         }
-        catch (Exception ex)
-        {
-            Console.Write("TIENES UN ERROR: " + ex.Message);
-        }
-        conexion.Close();
-        return listPersonal;
-    }
 }
--- sigutc/academic/private/reservalab/ReservaLaboratorioLab.aspx
+++ sigutc/academic/private/reservalab/ReservaLaboratorioLab.aspx
@@ -7,5 +7,38 @@
 <asp:Content ID="Content3" ContentPlaceHolderID="MainContent" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content4" ContentPlaceHolderID="FooterContent" Runat="Server">
+      <script type="text/javascript">
+        document.addEventListener('DOMContentLoaded', function () {
+            var calendarEl = document.getElementById('calendar');
+
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                buttonText: {
+                    month: 'Mes',
+                    week: 'Semana',
+                    list: 'Lista'
+                },
+                initialView: 'dayGridMonth', // vista mensual
+                locale: 'es',                // idioma español
+                headerToolbar: {
+                    left: 'prev,next',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,listWeek'
+                },
+                dayHeaderContent: function (arg) {
+                    switch (arg.date.getUTCDay()) {
+                        case 0: return 'Domingo';
+                        case 1: return 'Lunes';
+                        case 2: return 'Martes';
+                        case 3: return 'Miércoles';
+                        case 4: return 'Jueves';
+                        case 5: return 'Viernes';
+                        case 6: return 'Sábado';
+                    }
+                },
+            });
+
+            calendar.render();
+        });
+    </script>
 </asp:Content>
 
